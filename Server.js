b5b423fa978b435301f30e305898a222c7e8d606
Girik Soni <giriksoni0710>
2019-07-10 03:14:48 -0700

    Backend-mongoose-api-data

---
+++ Server.js
@@ -0,0 +1,143 @@
+const fs = require('fs');
+
+const express = require('express');
+const bodyparser = require('body-parser');
+
+const cors = require('cors');
+
+const app = express();
+
+app.use(cors());
+app.use(bodyparser.json());
+
+
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost/HospiCheck', { useNewUrlParser: true });
+
+var db = mongoose.connection;
+
+db.on('error', ()=>{
+
+console.log("error");
+
+})
+
+db.once('open', ()=>{
+
+console.log("connected to mongodb");
+
+})
+
+
+//Hospital Data Insert
+
+const hospital = mongoose.model('hospital', {
+
+    hospName: String,
+    hospAddr: String,
+    city: String,
+    phone: Number,
+    type: String
+
+});
+
+
+
+const readdata = fs.readFileSync('json.json');
+
+    const data = JSON.parse(readdata);
+
+    let objjson, obj;
+
+    
+   
+    data.records.forEach(element => {
+        
+        // let count = 0;
+       
+        
+const hospitalDataList = new hospital(
+    {
+
+        hospName: element.SV_NAME,
+    hospAddr: element.STREET_NUMBER, 
+    city: element.CITY,
+   phone: element.PHONE_NUMBER,
+   type: "hospital"
+   
+
+});
+
+        hospitalDataList.save();
+
+    
+    });
+    
+
+    //Clinics data insert
+
+
+    const clinics = mongoose.model('clinics', {
+
+        
+    clinicName: String,
+    clinicAddr: String,
+    city: String,
+    phone: Number,
+    type: String
+
+
+    })
+
+    const readdata2 = fs.readFileSync('clinics.json');
+
+    const data2 = JSON.parse(readdata2);
+
+
+    data2.records.forEach(elements => {
+        
+
+        const clinicslist = new clinics({
+
+
+            
+    clinicName: elements.RG_NAME,
+    clinicAddr: elements.STREET_NUMBER,
+    city: elements.CITY,
+    phone: elements.PHONE_NUMBER,
+    type: "Walkinclinics"
+
+
+        })
+
+        clinicslist.save();
+
+
+
+
+    });
+    
+
+
+
+
+
+
+
+  
+
+
+
+     
+
+
+
+    
+
+
+app.listen(3000, function(){
+
+
+    console.log("Server Running");
+})
